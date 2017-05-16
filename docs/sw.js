8783322ada6a47cba4b6082a84eb3010cf505898
houseofkodai <karthik@houseofkodai.in>
2017-05-16 10:59:24 +0530

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,40 @@
+var APP_PREFIX = 'ICX'
+var VERSION = '0.1'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  '/icx/',
+  '/icx/index.html'
+]
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request)
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME)
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
